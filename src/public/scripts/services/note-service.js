694cf0e48bc452563317e16a4607b91ed3bd4c1f
amir_g <info@ghani.ch>
2021-06-08 18:40:09 +0200

    add styling cleanup

--- src/public/scripts/services/note-service.js
+++ src/public/scripts/services/note-service.js
@@ -1,20 +1,15 @@
+import {createDate} from '../utils.js';
+
 class NoteService {
     constructor(initialNotes) {
         this.notes = initialNotes;
-    }
-
-    createDate(date) {
-        const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-        const splitDate = date.split('-');
-        const dat = new Date(splitDate[0], (splitDate[1] - 1), splitDate[2]);
-        return weekDays[dat.getDay()];
     }
 
     addNote({date = 0, title = '', importance = 1, description = ''}) {
         const newNote = {
             id: this.notes.length > 0 ? this.notes[this.notes.length - 1].id + 1 : 1,
             dueDay: {
-                weekday: this.createDate(date),
+                weekday: createDate(date),
                 date,
             },
             title,
@@ -38,7 +33,6 @@ class NoteService {
             this.notes[indexOfEditNote].importance = editNote.importance;
         }
         this.notes[indexOfEditNote].dueDay.date = editNote.date;
-
         this.onNotesChanged(this.notes);
     }
 
